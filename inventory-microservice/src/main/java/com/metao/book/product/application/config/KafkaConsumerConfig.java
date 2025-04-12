8b784e53f4c79c056556f6a0fe98eff894258161
mehrdad.karami <mehrdadallahkarami@gmail.com>
2025-04-13 00:21:34 +0200

    updated tests

--- inventory-microservice/src/main/java/com/metao/book/product/application/config/KafkaConsumerConfig.java
+++ inventory-microservice/src/main/java/com/metao/book/product/application/config/KafkaConsumerConfig.java
@@ -17,8 +17,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.DefaultErrorHandler;
 
 @EnableKafka
 @Configuration
--- shared-test/src/main/java/com/metao/shared/test/BaseKafkaIT.java
+++ shared-test/src/main/java/com/metao/shared/test/BaseKafkaIT.java
@@ -1,20 +1,18 @@
 package com.metao.shared.test;
 
-import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @Slf4j
-@UtilityClass
 @Testcontainers
 public class BaseKafkaIT {
 
-    private static final ConfluentKafkaContainer kafkaContainer = new ConfluentKafkaContainer(
+    private static final KafkaContainer kafkaContainer = new KafkaContainer(
         DockerImageName.parse("confluentinc/cp-kafka:7.7.0")
             .asCompatibleSubstituteFor("apache/kafka"));
 
