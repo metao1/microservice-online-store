4b77909b0ec7278e922749e57c80bcb54e5e9c10
mehrdad.karami <mehrdadallahkarami@gmail.com>
2026-02-06 15:08:49 +0100

    order-microservice: upgraded wiremock

--- payment-microservice/src/main/java/com/metao/book/payment/PaymentApplication.java
+++ payment-microservice/src/main/java/com/metao/book/payment/PaymentApplication.java
@@ -2,16 +2,7 @@ package com.metao.book.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
-@ComponentScan(
-    basePackages = {"com.metao.book.payment", "com.metao.kafka"},
-    excludeFilters = @ComponentScan.Filter(
-        type = FilterType.ASSIGNABLE_TYPE,
-        classes = {com.metao.kafka.KafkaEventConfiguration.class}
-    )
-)
 @SpringBootApplication
 public class PaymentApplication {
 
