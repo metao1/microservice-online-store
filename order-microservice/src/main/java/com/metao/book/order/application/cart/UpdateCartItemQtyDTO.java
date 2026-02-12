a6480871937a32295a645cbc182bcbbb9810c3d3
mehrdad.karami <mehrdadallahkarami@gmail.com>
2026-02-12 23:22:56 +0100

    BE: updated CartItem and Product DTO validation

--- order-microservice/src/main/java/com/metao/book/order/application/cart/UpdateCartItemQtyDTO.java
+++ order-microservice/src/main/java/com/metao/book/order/application/cart/UpdateCartItemQtyDTO.java
@@ -1,6 +1,13 @@
 package com.metao.book.order.application.cart;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-public record UpdateCartItemQtyDTO(BigDecimal quantity) {
+@Valid
+public record UpdateCartItemQtyDTO(
+    @NotNull(message = "quantity cannot be null")
+    @DecimalMin(value = "0.00", message = "quantity must be greater than or equal to zero")
+    BigDecimal quantity) {
 }
--- order-microservice/src/main/java/com/metao/book/order/application/config/KafkaConfig.java
+++ order-microservice/src/main/java/com/metao/book/order/application/config/KafkaConfig.java
@@ -23,6 +23,11 @@ public class KafkaConfig {
         return createTopic(kafkaTopic);
     }
 
+    @Bean
+    public NewTopic orderUpdatedTopic(@Value("${kafka.topic.order-updated.name}") String kafkaTopic) {
+        return createTopic(kafkaTopic);
+    }
+
     private static NewTopic createTopic(String topic) {
         return TopicBuilder
             .name(topic)
