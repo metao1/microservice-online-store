f4a05c9734b5f88449302be1282611154ba2beb7
mehrdad.karami <mehrdadallahkarami@gmail.com>
2026-02-15 13:13:18 +0100

    Revert "BE: updated CartItem and Product DTO validation"
    This reverts commit a6480871937a32295a645cbc182bcbbb9810c3d3.

--- order-microservice/src/main/java/com/metao/book/order/presentation/dto/CreateOrderRequestDTO.java
+++ order-microservice/src/main/java/com/metao/book/order/presentation/dto/CreateOrderRequestDTO.java
@@ -1,10 +1,8 @@
 package com.metao.book.order.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
-@Valid
 public record CreateOrderRequestDTO(@NotBlank(message = "User ID cannot be blank")
                                     @JsonProperty("user_id")
                                     String userId) {
