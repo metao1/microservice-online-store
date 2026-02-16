5b7d1f87b02df0b4a565d184a080a4e935348668
mehrdad.karami <mehrdadallahkarami@gmail.com>
2026-02-16 16:34:01 +0100

    BE: Opened Cors to every domain name temporarily for testing purposes for payment-microservice

---
+++ payment-microservice/src/main/java/com/metao/book/payment/application/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.metao.book.payment.application.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow CORS for all paths
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allowed HTTP methods
+            .allowedHeaders("*")  // Allow any headers
+            .allowCredentials(true);  // Allow credentials (e.g., cookies)
+    }
+}
