88d5e6f87b425f81231aeb9035d9494928b7c76b
mehrdad.karami <mehrdadallahkarami@gmail.com>
2026-02-16 17:48:33 +0100

    BE: provide default image url if image is empty and default currency as a Tech Debt

--- inventory-microservice/src/main/java/com/metao/book/product/application/mapper/ProductApplicationMapper.java
+++ inventory-microservice/src/main/java/com/metao/book/product/application/mapper/ProductApplicationMapper.java
@@ -4,12 +4,18 @@ import com.metao.book.product.application.dto.CreateProductDto;
 import com.metao.book.product.application.dto.ProductDTO;
 import com.metao.book.product.domain.model.aggregate.Product;
 import com.metao.book.product.domain.model.entity.ProductCategory;
-import com.metao.book.product.domain.model.valueobject.*;
+import com.metao.book.product.domain.model.valueobject.CategoryName;
+import com.metao.book.product.domain.model.valueobject.ImageUrl;
+import com.metao.book.product.domain.model.valueobject.ProductDescription;
+import com.metao.book.product.domain.model.valueobject.ProductSku;
+import com.metao.book.product.domain.model.valueobject.ProductTitle;
+import com.metao.book.product.domain.model.valueobject.ProductVolume;
 import com.metao.book.shared.domain.financial.Money;
-import org.springframework.stereotype.Component;
 import java.time.Instant;
+import java.util.Currency;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
 
 /**
  * Mapper between domain objects and application DTOs
@@ -43,7 +49,8 @@ public class ProductApplicationMapper {
             ProductTitle.of(productDTO.title()),
             ProductDescription.of(productDTO.description()),
             ProductVolume.of(productDTO.volume()),
-            new Money(productDTO.currency(), productDTO.price()),
+            new Money(productDTO.currency() == null ?
+                Currency.getInstance("EUR") : productDTO.currency(), productDTO.price()),
             createdTime,
             createdTime,
             ImageUrl.of(productDTO.imageUrl()),
@@ -64,7 +71,10 @@ public class ProductApplicationMapper {
             new Money(createProductDto.currency(), createProductDto.price()),
             createdTime,
             createdTime,
-            ImageUrl.of(createProductDto.imageUrl()),
+            ImageUrl.of(
+                createProductDto.imageUrl() == null
+                    ? "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=400&h=500&fit=crop" :
+                    createProductDto.imageUrl()),
             createProductDto.categories().stream()
                 .map(CategoryName::of)
                 .map(ProductCategory::of)
