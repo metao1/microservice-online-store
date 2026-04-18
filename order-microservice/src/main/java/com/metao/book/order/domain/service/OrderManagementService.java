054b8c228d290d71da27b5fc1d1006bf172a1b4d
Mehrdad Karami <metao1@users.noreply.github.com>
2026-04-18 23:52:38 +0200

    Update order-microservice/src/main/java/com/metao/book/order/domain/service/OrderManagementService.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- order-microservice/src/main/java/com/metao/book/order/domain/service/OrderManagementService.java
+++ order-microservice/src/main/java/com/metao/book/order/domain/service/OrderManagementService.java
@@ -21,7 +21,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-;
+@Service
 
 @Service
 @RequiredArgsConstructor
