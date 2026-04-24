85843f213f3be872c59f374ba4cbcfb079460d1f
Mehrdad Karami <metao1@users.noreply.github.com>
2026-04-25 01:21:59 +0200

    Update shared-kernel/src/main/java/com/metao/book/shared/domain/financial/VAT.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- shared-kernel/src/main/java/com/metao/book/shared/domain/financial/VAT.java
+++ shared-kernel/src/main/java/com/metao/book/shared/domain/financial/VAT.java
@@ -93,6 +93,8 @@ public class VAT implements ValueObject {
         var tax = amount.fixedPointAmount()
             .multiply(BigDecimal.valueOf(percentage))
             .divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
+            .multiply(BigDecimal.valueOf(percentage))
+            .divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
         return Money.of(amount.currency(), tax);
     }
 
